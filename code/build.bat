32583bde45071c11c39f12eadbd71854b2642bb7
Nahuel Lattessi <n_lattessi@hotmail.com>
2016-10-17 21:59:58 -0300

    end 1

--- code/build.bat
+++ code/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-mkdir ..\..\build
-pushd ..\..\build
-cl -Zi ..\code\win32_handmade.cpp
+mkdir ..\build
+pushd ..\build
+cl -Zi ..\code\win32_handmade.cpp user32.lib
 popd
--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -6,5 +6,7 @@ WinMain(HINSTANCE hInstance,
 		LPSTR lpCmdLine,
 		int nCmdShow)
 {
+	MessageBoxA(0, "This is Handmade Hero", "Handmade Hero", MB_OK|MB_ICONINFORMATION);
+
 	return(0);
 }
